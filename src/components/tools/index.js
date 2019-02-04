2039fdc52ce3e750ec45991ff30b6df8f54a531f
Austin Lauritsen <austin@veryaustin.com>
2019-02-04 16:27:03 -0600

    Update Tools component TODO

--- src/components/tools/index.js
+++ src/components/tools/index.js
@@ -1,4 +1,5 @@
 import Tools from './components/Tools'
 
 // TODO: Refactor this and associated components into NPM package
+// TODO: Implement hyperlinks to project sites on click
 export { Tools }
