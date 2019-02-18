a182b340e52951a9fa48e8c88d0bea0309991958
Austin Lauritsen <austin@veryaustin.com>
2019-02-18 17:17:40 -0600

    Fix text wrap issues on layouts other than Desktop

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
+import { device } from '../styles/mediaQueries'
 
 const Article = styled.article`
   height: calc(100vh - 140px);
   display: grid;
   align-items: center;
+  max-width: 90%;
   code {
     font-family: 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
     font-size: 3.25em;
@@ -15,6 +17,9 @@ const Article = styled.article`
       position: relative;
       left: 10%;
     }
+  }
+  @media ${device.desktop} {
+    max-width: 100%;
   }
 `
 
